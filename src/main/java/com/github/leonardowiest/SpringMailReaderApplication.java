05a03a258e715f49c157bfb305d21badc5b2742c
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-13 20:26:15 -0300

    Doc: Code

---
+++ src/main/java/com/github/leonardowiest/SpringMailReaderApplication.java
@@ -0,0 +1,13 @@
+package com.github.leonardowiest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leonardowiest/SpringMailReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leonardowiest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
