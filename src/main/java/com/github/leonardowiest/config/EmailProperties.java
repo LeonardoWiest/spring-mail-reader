6ccfded78c8eb189900b22c7a31a7709243f6349
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-15 19:06:50 -0300

    Doc: Carregamento de Properties chave e valor

--- src/main/java/com/github/leonardowiest/config/EmailProperties.java
+++ src/main/java/com/github/leonardowiest/config/EmailProperties.java
@@ -16,8 +16,14 @@ public class EmailProperties extends MailProperties {
 	public Properties loadProperties() {
 
 		Properties prop = new Properties();
+
+		prop.put("spring.mail.host", super.getHost());
+		prop.put("spring.mail.port", super.getPort());
 		prop.put("spring.mail.username", super.getUsername());
 		prop.put("spring.mail.password", super.getPassword());
+		prop.put("spring.mail.default-encoding", super.getDefaultEncoding());
+		prop.put("spring.mail.properties.*", super.getProperties());
+		prop.put("spring.mail.jndi-name", super.getJndiName());
 
 		return prop;
 	}
