2376419b17f6a178edf1ef4ec2c77375e2f272bb
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-14 22:16:26 -0300

    Doc: Carregamento de Properties chave e valor

--- src/main/java/com/github/leonardowiest/config/EmailProperties.java
+++ src/main/java/com/github/leonardowiest/config/EmailProperties.java
@@ -3,7 +3,6 @@ package com.github.leonardowiest.config;
 import java.util.Properties;
 
 import org.springframework.boot.autoconfigure.mail.MailProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
  * @author Leonardo Wiest
  *
  */
-@ConfigurationProperties(prefix = "spring.mail")
 @Configuration
 public class EmailProperties extends MailProperties {
 
